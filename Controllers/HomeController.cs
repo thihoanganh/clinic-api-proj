dadf96acaafc9cfb257167d9bb8271b838abdf90
Thi HoangAnh <thihoanganh.it@gmail.com>
2021-07-29 11:26:38 +0700

    init api

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
